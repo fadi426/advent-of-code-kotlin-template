f80860bdbf983b835353c6aab4dcb1ce3dfddbcb
ia88sj <fadi.qadadi@ing.com>
2021-12-06 20:41:37 +0100

    Day06

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -6,19 +6,20 @@ import day01.readDayInput
 
 fun main() {
     fun calcLanternFishDensity(input: List<Int>, days: Int): Long {
-        val countList = (0..9).map { n -> input.count { n == it } }.map { it.toLong() }.toMutableList()
+        var countList = (0..9).map { n -> input.count { n == it } }.map { it.toLong() }.toMutableList()
 
         for (i in 1..days) {
-            val firstFish: Long = countList[0]
-            countList[0] = countList[1]
-            countList[1] = countList[2]
-            countList[2] = countList[3]
-            countList[3] = countList[4]
-            countList[4] = countList[5]
-            countList[5] = countList[6]
-            countList[6] = countList[7] + firstFish
-            countList[7] = countList[8]
-            countList[8] = firstFish
+            countList = mutableListOf(
+                countList[1],
+                countList[2],
+                countList[3],
+                countList[4],
+                countList[5],
+                countList[6],
+                countList[7] + countList[0],
+                countList[8],
+                countList[0]
+            )
         }
         return countList.sum()
     }
