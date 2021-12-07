6b517e1de61e3e1470c29a72c947bfdcf944fd27
ia88sj <fadi.qadadi@ing.com>
2021-12-07 10:36:32 +0100

    Day07

---
+++ src/day07/Day07.kt
@@ -0,0 +1,32 @@
+package day01.day07
+
+import day01.assertTrue
+import day01.readDayInput
+import kotlin.math.absoluteValue
+
+fun main() {
+
+    fun task01(input: List<Int>): Int {
+        var cheapestOutcome = Int.MAX_VALUE
+        for (i in 0..input.maxOrNull()!!) {
+            var maxValue = 0
+            for (j in input.indices) maxValue += (input[j] - i).absoluteValue
+            if (maxValue < cheapestOutcome) cheapestOutcome = maxValue.absoluteValue
+        }
+        return cheapestOutcome
+    }
+
+    fun task02(input: List<Int>): Int {
+        var cheapestOutcome = Int.MAX_VALUE
+        for (i in 0..input.maxOrNull()!!) {
+            var maxValue = 0
+            for (j in input.indices) maxValue += (input[j] - i).absoluteValue * ((input[j] - i).absoluteValue+1)/2
+            if (maxValue < cheapestOutcome) cheapestOutcome = maxValue.absoluteValue
+        }
+        return cheapestOutcome
+    }
+
+    val input = readDayInput("Day07").first().split(",").map { it.toInt() }
+    assertTrue(task01(input) == 356992)
+    assertTrue(task02(input) == 101268110)
+}
